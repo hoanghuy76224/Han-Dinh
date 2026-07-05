da124b456dd34daad0519f478e3be545071cdf17
hoanghuy76224 <ntd031096@gmail.com>
2026-07-06 06:33:11 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase config - dán các giá trị anh copy từ Firebase Console vào đây
+// Xem SETUP.md để biết cách lấy các giá trị này
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Tên collection lưu RSVP trong Firestore (không cần đổi)
+export const RSVP_COLLECTION = "rsvps";
